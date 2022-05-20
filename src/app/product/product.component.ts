140e3d721d4ecde817e6dbb67310b1d25c69fc19
kkevin203 <binokev@hotmail.com>
2022-05-20 21:14:32 +0200

    création service

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,20 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { SellService } from '../services/sell.service';
 
 @Component({
   selector: 'wfb-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
-  title!:string;
+  title!: string;
   description!: string;
   price!: number;
-  constructor() { }
+  constructor(private sell: SellService) {}
 
   ngOnInit(): void {
-    this.title= 'Maillot'
-    this.description= 'voici notre maillot'
-    this.price= 45
+    this.title = 'Maillot';
+    this.description = 'voici notre maillot';
+    this.price = 45;
   }
-
+  sellProduct() {
+    this.sell.state = 'selled';
+  }
+  showProduct() {
+    alert(this.sell.getState());
+  }
 }
