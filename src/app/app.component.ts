447b98752c4e4d46f2a318d0a6e9e29d4ca73da4
kkevin203 <binokev@hotmail.com>
2022-05-27 21:03:42 +0200

    up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
 @Component({
   selector: 'wfb-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'esport';
+  tiles: Tile[] = [
+    { text: 'One', cols: 1, rows: 1, color: 'lightblue' },
+    { text: 'Two', cols: 1, rows: 1, color: 'lightgreen' },
+    { text: 'Three', cols: 1, rows: 1, color: 'lightpink' },
+    { text: 'Four', cols: 1, rows: 1, color: '#DDBDF1' },
+  ];
+  campaignOne: FormGroup;
+  campaignTwo: FormGroup;
+
+  constructor() {
+    const today = new Date();
+    const month = today.getMonth();
+    const year = today.getFullYear();
+
+    this.campaignOne = new FormGroup({
+      start: new FormControl(new Date(year, month, 13)),
+      end: new FormControl(new Date(year, month, 16)),
+    });
+
+    this.campaignTwo = new FormGroup({
+      start: new FormControl(new Date(year, month, 15)),
+      end: new FormControl(new Date(year, month, 19)),
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,15 @@ import { ProductComponent } from './product/product.component';
 import { LoginComponent } from './login/login.component';
 import { ListProductComponent } from './list-product/list-product.component';
 import { SubscriptionComponent } from './subscription/subscription.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatDatepickerModule  } from '@angular/material/datepicker';
+import { MatFormFieldModule  } from '@angular/material/form-field';
+
+
 
 @NgModule({
   declarations: [
@@ -14,13 +23,21 @@ import { SubscriptionComponent } from './subscription/subscription.component';
     ProductComponent,
     LoginComponent,
     ListProductComponent,
-    SubscriptionComponent
+    SubscriptionComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatButtonModule,
+    MatIconModule,
+    MatGridListModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  exports: [ProductComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
